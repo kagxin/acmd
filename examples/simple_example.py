91c025001a0000d1be215d4326402b0502334327
康鑫 <k506855660@163.com>
2017-10-16 23:09:38 +0800

    add a simple example

---
+++ examples/simple_example.py
@@ -0,0 +1,21 @@
+import asyncio
+import sys
+from acmd import Cmd
+
+class FooCmd(Cmd):
+    prompt = '(test)'
+
+    def do_hello(self, arg):
+        print('hello:{}'.format(arg))
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    foo = FooCmd()
+
+    foo.cmdloop(loop=loop)
+
+    try:
+        loop.run_forever()  # our cmd will run automatically from this moment
+    except KeyboardInterrupt:
+        loop.stop()
